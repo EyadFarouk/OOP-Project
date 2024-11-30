32f4d7202e9812dd208bd71f463b6eaec4b7f89c
priceelio <faroukeyad11@gmail.com>
2024-11-30 20:26:20 +0200

    Improved Person class Added More attributes

--- src/person/Person.java
+++ src/person/Person.java
@@ -3,9 +3,14 @@ package person;
 import java.util.Scanner;
 
 abstract public class Person {
-    String name;
-    String password;
+    String Fname;
+    String Lname;
     String email;
+    int Phone;
+    int age;
+    String gender;
+    String address;
+    String password;
     public void login(){
         System.out.println("Welcome in Log in page");
         Scanner scanner=new Scanner(System.in);
@@ -18,10 +23,20 @@ abstract public class Person {
     public void signup(){
         System.out.println("Welcome in sign up page");
         Scanner scanner=new Scanner(System.in);
-        System.out.println("Please enter your name : ");
-        this.name=scanner.next();
+        System.out.println("Please enter your first name : ");
+        this.Fname=scanner.next();
+        System.out.println("Please enter your last name : ");
+        this.Lname=scanner.next();
         System.out.println("Please enter your email : ");
         this.email=scanner.next();
+        System.out.println("Please enter your phone number : ");
+        this.Phone=scanner.nextInt();
+        System.out.println("Please enter your age : ");
+        this.age=scanner.nextInt();
+        System.out.println("Please enter your gender : ");
+        this.gender=scanner.next();
+        System.out.println("Please enter your address : ");
+        this.address=scanner.next();
         System.out.println("Please enter your password : ");
         this.password=scanner.next();
     }
