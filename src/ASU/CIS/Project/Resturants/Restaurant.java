d166c4ffc99cef65113fc5b1d9da8af72e6e0ca7
mohamedtaalat <mohamedabotalat057@gmail.com>
2024-12-02 15:10:50 -0800

    some addition in restaurant and menu {compare and test file to save data but i don't complete file for Restaurant}

--- src/ASU/CIS/Project/Resturants/Restaurant.java
+++ src/ASU/CIS/Project/Resturants/Restaurant.java
@@ -1,14 +1,18 @@
 package ASU.CIS.Project.Resturants;
 
+import ASU.CIS.Project.Person.Customer;
 
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
 
-public class Restaurant extends Menu {
+public class Restaurant extends Menu implements Comparable<Restaurant> {
 
     public String name;
     public String address;
     public String contactInformation;
     public double rating;
-    int numberOfRating;
     public void displayMenu(){
         for (int i=0;i<10;i++){
             System.out.println("name of item "+(i+1) +" : "+menu.get(i).name);
@@ -49,5 +53,33 @@ public class Restaurant extends Menu {
         }
     }
 
+    @Override
+    public int compareTo(Restaurant o) {
+        if (this.rating>o.rating){
+            return 1;
+        }else if (this.rating<o.rating){
+            return -1;
+        }
+        return 0;
+    }
+    public void saveData(List<Restaurant> restaurantList) {
+        FileWriter fileWriter;
+        try {
+            fileWriter = new FileWriter("Data/restaurantdata.csv");
+            fileWriter.write("name,address,contact info,rating,name dish1,descripiton1,price1,categories1,rating1,name dish2,descripiton2,price2,categries2,rating2\n");
 
+               for (Restaurant restaurant :restaurantList) {
+                   fileWriter.append(restaurant.toString());
+               }
+
+            fileWriter.close();
+        }
+        catch (IOException e){
+            throw new RuntimeException(e);
+        }
+
+    }
+    public String toString() {
+        return this.name + ',' + this.address + ',' + this.contactInformation + ',' + this.rating + ',' + this.menu +'\n';
+    }
 }
