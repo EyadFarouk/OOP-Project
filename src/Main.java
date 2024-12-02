ad998777ec0d35ef9e302f38f954eb0ca481809a
priceelio <faroukeyad11@gmail.com>
2024-12-02 20:42:27 +0200

    Tests but I prefer keeping them

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,38 @@
 import ASU.CIS.Project.UI.*;
 import ASU.CIS.Project.Person.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
 
     public static void main(String[] args) {
-
+        Customer customer = new Customer();
+        // A function to give fake data to test on
+            customer.instance();
+        //------------------------------
         Ui ui=new Ui();
 
         int choose= ui.firstPage();
 
         if (choose==1){
 
-            Customer customer =new Customer();
-
             choose=ui.loginOrSignup();
+            Customer customer1;
 
             if(choose==1){
-                customer.login();
+               customer1= customer.login();
+               customer1.displayUserInfo();
             }
 
             else if (choose==2){
                 customer.signup();
+                customer1= customer.login();
+                customer1.displayUserInfo();
+                customer.displayUserInfo();
             }
 
             ui.homePage();
