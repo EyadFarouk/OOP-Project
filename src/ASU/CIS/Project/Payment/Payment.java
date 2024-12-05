77de31bcd16c22444ea5e95fe7f9908fb3dfdfd1
el_code_da3 <159466028+Batcatman0@users.noreply.github.com>
2024-12-05 08:22:54 +0200

    Merge remote-tracking branch 'origin/master'

--- src/ASU/CIS/Project/Payment/Payment.java
+++ src/ASU/CIS/Project/Payment/Payment.java
@@ -1,7 +1,32 @@
 package ASU.CIS.Project.Payment;
 
 public class Payment {
-    public boolean cart;
-    public String transictionId;
-    public String paymentStatus;
+    private String TransactionId;
+    private String PaymentStatus;
+
+    public String getPaymentStatus() {
+        return PaymentStatus;
+    }
+
+    public void setPaymentStatus(String paymentStatus) {
+        this.PaymentStatus = paymentStatus;
+    }
+
+    /*public String CheckOut(card cardManager) {
+        card selectedCard = cardManager.SelectCard();
+        if (selectedCard == null) {
+            setPaymentStatus("Failed");
+            return "Payment Failed";
+        }
+
+        if (selectedCard.getMoneyAvailable() >= totalPrice) {
+            selectedCard.setMoneyAvailable(selectedCard.getMoneyAvailable() - totalPrice);
+            setPaymentStatus("Success");
+            return "Payment Successful";
+        } else {
+            setPaymentStatus("Failed");
+            return "Payment Failed";
+        }
+    }*/
 }
+
