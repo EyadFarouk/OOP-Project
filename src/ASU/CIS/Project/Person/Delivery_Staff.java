0d776130b1d6fb8590d8308dc45938d14ff424af
Mohyeldin <mohie.yakout@gmail.com>
2024-12-09 15:04:57 +0200

    initial delivery staff class

---
+++ src/ASU/CIS/Project/Person/Delivery_Staff.java
@@ -0,0 +1,23 @@
+package ASU.CIS.Project.Person;
+
+import ASU.CIS.Project.Interfaces.saveAndLoad;
+
+
+
+
+public class Delivery_Staff extends User implements saveAndLoad {
+    @Override
+    public void saveData() {
+
+    }
+
+    @Override
+    public void loadData() {
+
+    }
+
+    @Override
+    public User login() {
+        return null;
+    }
+}
